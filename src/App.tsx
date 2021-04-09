1193d1e1e370f690403a3e3acda0f0db56a0f3f9
Edwin <urbaniak_edwin@wp.pl>
2021-04-09 17:58:35 +0200

    install Apollo Client

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import client from "./api/client";
+import { ApolloProvider } from "@apollo/client";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>App</h1>
+      </div>
+      ;
+    </ApolloProvider>
   );
 }
 
